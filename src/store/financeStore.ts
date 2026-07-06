9dd1ddaed2d6e0d07cc971c2875a5ee2fa1341ed
Gurkan Aygun <gurkan@local.dev>
2026-07-06 17:20:32 +0300

    feat: Enforce local data persistence using Zustand persist middleware and AsyncStorage

--- src/store/financeStore.ts
+++ src/store/financeStore.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { Expense, FinancePlan, Income, Period, SavingsGoal } from "@/models/finance";
 import {
@@ -124,126 +126,139 @@ function createPlan(incomes: Income[], expenses: Expense[], savingsGoal: Savings
   };
 }
 
-export const useFinanceStore = create<FinanceState>()((set, get) => ({
-  hasHydrated: true,
-  incomes: initialIncomes,
-  expenses: initialFixedExpenses,
-  savingsGoal: initialGoal,
-  selectedPeriod: initialPeriod,
-  plan: initialPlan,
-  isDarkMode: false,
-  setIsDarkMode: (isDarkMode) => set({ isDarkMode }),
-  isHapticsEnabled: true,
-  setIsHapticsEnabled: (isHapticsEnabled) => set({ isHapticsEnabled }),
-  language: "tr",
-  setLanguage: (language) => set({ language }),
-  setIncomes: (incomes) => {
-    const normalizedIncomes = incomes.map(normalizeIncome);
-    const { expenses, savingsGoal, selectedPeriod } = get();
-    const next = createPlan(normalizedIncomes, expenses, savingsGoal, selectedPeriod);
+export const useFinanceStore = create<FinanceState>()(
+  persist(
+    (set, get) => ({
+      hasHydrated: false,
+      incomes: initialIncomes,
+      expenses: initialFixedExpenses,
+      savingsGoal: initialGoal,
+      selectedPeriod: initialPeriod,
+      plan: initialPlan,
+      isDarkMode: false,
+      setIsDarkMode: (isDarkMode) => set({ isDarkMode }),
+      isHapticsEnabled: true,
+      setIsHapticsEnabled: (isHapticsEnabled) => set({ isHapticsEnabled }),
+      language: "tr",
+      setLanguage: (language) => set({ language }),
+      setIncomes: (incomes) => {
+        const normalizedIncomes = incomes.map(normalizeIncome);
+        const { expenses, savingsGoal, selectedPeriod } = get();
+        const next = createPlan(normalizedIncomes, expenses, savingsGoal, selectedPeriod);
 
-    console.log("[finance-store] setIncomes", {
-      incomes: normalizedIncomes,
-      totalIncome: calculateTotalIncome(normalizedIncomes),
-      fixedExpenses: expenses.filter((expense) => expense.isFixed),
-      totalFixedExpenses: calculateTotalFixedExpenses(expenses),
-      monthlyRemaining: next.plan.monthlyRemaining
-    });
+        console.log("[finance-store] setIncomes", {
+          incomes: normalizedIncomes,
+          totalIncome: calculateTotalIncome(normalizedIncomes),
+          fixedExpenses: expenses.filter((expense) => expense.isFixed),
+          totalFixedExpenses: calculateTotalFixedExpenses(expenses),
+          monthlyRemaining: next.plan.monthlyRemaining
+        });
 
-    set({
-      incomes: normalizedIncomes,
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  setExpenses: (expenses) => {
-    const normalizedExpenses = expenses.map(normalizeExpense);
-    const { incomes, savingsGoal, selectedPeriod } = get();
-    const next = createPlan(incomes, normalizedExpenses, savingsGoal, selectedPeriod);
+        set({
+          incomes: normalizedIncomes,
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      setExpenses: (expenses) => {
+        const normalizedExpenses = expenses.map(normalizeExpense);
+        const { incomes, savingsGoal, selectedPeriod } = get();
+        const next = createPlan(incomes, normalizedExpenses, savingsGoal, selectedPeriod);
 
-    set({
-      expenses: normalizedExpenses,
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  setFixedExpenses: (fixedExpenses) => {
-    const normalizedFixedExpenses = fixedExpenses.map((expense) => normalizeExpense({ ...expense, isFixed: true }));
-    const variableExpenses = get().expenses.filter((expense) => !expense.isFixed);
-    const expenses = [...normalizedFixedExpenses, ...variableExpenses];
-    const { incomes, savingsGoal, selectedPeriod } = get();
-    const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
+        set({
+          expenses: normalizedExpenses,
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      setFixedExpenses: (fixedExpenses) => {
+        const normalizedFixedExpenses = fixedExpenses.map((expense) => normalizeExpense({ ...expense, isFixed: true }));
+        const variableExpenses = get().expenses.filter((expense) => !expense.isFixed);
+        const expenses = [...normalizedFixedExpenses, ...variableExpenses];
+        const { incomes, savingsGoal, selectedPeriod } = get();
+        const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
 
-    console.log("[finance-store] setFixedExpenses", {
-      incomes,
-      totalIncome: calculateTotalIncome(incomes),
-      fixedExpenses: normalizedFixedExpenses,
-      totalFixedExpenses: calculateTotalFixedExpenses(normalizedFixedExpenses),
-      monthlyRemaining: next.plan.monthlyRemaining
-    });
+        console.log("[finance-store] setFixedExpenses", {
+          incomes,
+          totalIncome: calculateTotalIncome(incomes),
+          fixedExpenses: normalizedFixedExpenses,
+          totalFixedExpenses: calculateTotalFixedExpenses(normalizedFixedExpenses),
+          monthlyRemaining: next.plan.monthlyRemaining
+        });
 
-    set({
-      expenses,
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  addExpense: (expense) => {
-    const expenses = [normalizeExpense(expense), ...get().expenses];
-    const { incomes, savingsGoal, selectedPeriod } = get();
-    const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
+        set({
+          expenses,
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      addExpense: (expense) => {
+        const expenses = [normalizeExpense(expense), ...get().expenses];
+        const { incomes, savingsGoal, selectedPeriod } = get();
+        const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
 
-    set({
-      expenses,
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  setSavingsGoal: (savingsGoal) => {
-    const { incomes, expenses, selectedPeriod } = get();
-    const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
+        set({
+          expenses,
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      setSavingsGoal: (savingsGoal) => {
+        const { incomes, expenses, selectedPeriod } = get();
+        const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
 
-    set({
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  setMonthlySavings: (amount) => {
-    const { savingsGoal } = get();
-    get().setSavingsGoal({ ...savingsGoal, monthlyContribution: normalizeAmount(amount) });
-  },
-  setSelectedPeriod: (selectedPeriod) => {
-    const { incomes, expenses, savingsGoal } = get();
-    const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
+        set({
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      setMonthlySavings: (amount) => {
+        const { savingsGoal } = get();
+        get().setSavingsGoal({ ...savingsGoal, monthlyContribution: normalizeAmount(amount) });
+      },
+      setSelectedPeriod: (selectedPeriod) => {
+        const { incomes, expenses, savingsGoal } = get();
+        const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
 
-    set({
-      selectedPeriod,
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  setHasHydrated: () => undefined,
-  refreshPlan: () => {
-    const incomes = get().incomes.map(normalizeIncome);
-    const expenses = get().expenses.map(normalizeExpense);
-    const { savingsGoal, selectedPeriod } = get();
-    const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
+        set({
+          selectedPeriod,
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      setHasHydrated: (hasHydrated) => set({ hasHydrated }),
+      refreshPlan: () => {
+        const incomes = get().incomes.map(normalizeIncome);
+        const expenses = get().expenses.map(normalizeExpense);
+        const { savingsGoal, selectedPeriod } = get();
+        const next = createPlan(incomes, expenses, savingsGoal, selectedPeriod);
 
-    set({
-      incomes,
-      expenses,
-      savingsGoal: next.savingsGoal,
-      plan: next.plan
-    });
-  },
-  getTotalIncome: () => calculateTotalIncome(get().incomes),
-  getTotalFixedExpenses: () => calculateTotalFixedExpenses(get().expenses),
-  getMonthlyRemaining: () => calculateMonthlyRemaining(get().incomes, get().expenses),
-  getSpendableMonthlyBudget: () => calculateSpendableMonthlyBudget(get().incomes, get().expenses, get().savingsGoal),
-  getDailyLimit: () => calculateDailyLimit(get().incomes, get().expenses, get().savingsGoal),
-  getWeeklyLimit: () => calculateWeeklyLimit(get().incomes, get().expenses, get().savingsGoal),
-  getMonthlyLimit: () => calculateMonthlyLimit(get().incomes, get().expenses, get().savingsGoal),
-  getExpensesTotalForPeriod: (period) => calculateExpensesTotalForPeriod(get().expenses, period),
-  getRemainingLimitForPeriod: (period) =>
-    calculateRemainingLimitForPeriod(get().incomes, get().expenses, get().savingsGoal, period)
-}));
+        set({
+          incomes,
+          expenses,
+          savingsGoal: next.savingsGoal,
+          plan: next.plan
+        });
+      },
+      getTotalIncome: () => calculateTotalIncome(get().incomes),
+      getTotalFixedExpenses: () => calculateTotalFixedExpenses(get().expenses),
+      getMonthlyRemaining: () => calculateMonthlyRemaining(get().incomes, get().expenses),
+      getSpendableMonthlyBudget: () => calculateSpendableMonthlyBudget(get().incomes, get().expenses, get().savingsGoal),
+      getDailyLimit: () => calculateDailyLimit(get().incomes, get().expenses, get().savingsGoal),
+      getWeeklyLimit: () => calculateWeeklyLimit(get().incomes, get().expenses, get().savingsGoal),
+      getMonthlyLimit: () => calculateMonthlyLimit(get().incomes, get().expenses, get().savingsGoal),
+      getExpensesTotalForPeriod: (period) => calculateExpensesTotalForPeriod(get().expenses, period),
+      getRemainingLimitForPeriod: (period) =>
+        calculateRemainingLimitForPeriod(get().incomes, get().expenses, get().savingsGoal, period)
+    }),
+    {
+      name: "birikim-yap-finance-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.setHasHydrated(true);
+        }
+      }
+    }
+  )
+);
